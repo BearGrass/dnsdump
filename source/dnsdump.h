57c245986e23a0be2509a215b19a3b676a8039e2
墨骨 <mogu.lwp@alibaba-inc.com>
2017-01-09 20:21:37 +0800

    时间测试demo

---
+++ source/dnsdump.h
@@ -0,0 +1,13 @@
+#ifndef __DNSDUMP_H__
+#define __DNSDUMP_H__
+
+/*
+ * Create at 19:07 1.9 2017 BearGrass
+ * email: superlong100@gmail.com
+ */
+
+typedef struct pacinfo_t {
+    int time;
+}Pacinfo;
+
+#endif
---
+++ source/timetest.c
@@ -0,0 +1,15 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *week[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    time_t timestamp;
+    struct tm *p;
+    time(&timestamp);
+    p = localtime(&timestamp);
+    printf("%d %d %d ", (1900 + p->tm_year), (1 + p->tm_mon), p->tm_mday);
+    printf("%s %d:%d:%d\n", week[p->tm_wday], p->tm_hour, p->tm_min, p->tm_sec);
+    printf("time = %s\n", ctime(&timestamp));
+    return 0;
+}
